6b26355c59c92ab78b5e55885c235eecebeba694
HaileB65 <brendon.haile@gmail.com>
2022-11-18 21:03:53 -0600

    First push

---
+++ src/main/java/Capstone/Project/NewMVCProject/NewMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package Capstone.Project.NewMVCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMvcProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Capstone/Project/NewMVCProject/NewMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.Project.NewMVCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f86c3b704b58438c9453283b2a3b3c2fb02d7aa9
HaileB65 <brendon.haile@gmail.com>
2023-01-17 23:41:25 -0600

    Cleaning up code.

--- src/main/java/Capstone/Project/VacationFinder/controllers/RestUserController.java
+++ src/main/java/Capstone/Project/VacationFinder/controllers/RestUserController.java
@@ -2,7 +2,6 @@ package Capstone.Project.VacationFinder.controllers;
 
 import Capstone.Project.VacationFinder.models.CodingNomadsAPI.apiUser;
 import Capstone.Project.VacationFinder.models.User;
-import Capstone.Project.VacationFinder.services.CodingNomadsAPIService;
 import Capstone.Project.VacationFinder.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,15 +11,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class RestUserController {
 
     @Autowired
     UserService userService;
-
-    @Autowired
-    CodingNomadsAPIService codingNomadsAPIService;
-
 
     @GetMapping
     public ResponseEntity<?> getAllUsers() {
@@ -40,10 +35,4 @@ public class RestUserController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
-    @GetMapping("/CNAPI")
-    public List<apiUser> getCNAPIUsers() {
-        return codingNomadsAPIService.getAllUsers();
-    }
-
 }

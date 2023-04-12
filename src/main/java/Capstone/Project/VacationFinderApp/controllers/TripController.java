8bc2bf072def8a9983fd937e521a77b1b27c09ff
HaileB65 <brendon.haile@gmail.com>
2023-04-12 17:46:01 -0500

    Updated destination page.

--- src/main/java/Capstone/Project/VacationFinderApp/controllers/TripController.java
+++ src/main/java/Capstone/Project/VacationFinderApp/controllers/TripController.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class TripController {

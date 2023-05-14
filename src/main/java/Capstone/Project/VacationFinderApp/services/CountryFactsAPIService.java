b419ec434baf738bdedeffc3e3f4cec85bef85f2
HaileB65 <brendon.haile@gmail.com>
2023-05-14 18:25:16 -0500

    Created get country facts method.

--- src/main/java/Capstone/Project/VacationFinderApp/services/CountryFactsAPIService.java
+++ src/main/java/Capstone/Project/VacationFinderApp/services/CountryFactsAPIService.java
@@ -27,28 +27,10 @@ public class CountryFactsAPIService {
 
         HttpEntity<Void> request = new HttpEntity<>(headers);
 
-        String url = "https://api.api-ninjas.com/v1/country?name=";
-        String param = "France";
+        String url = "https://api.api-ninjas.com/v1/country?name=France";
 
-        ResponseEntity<Country[]> response = restTemplate.exchange(url, HttpMethod.GET, request, Country[].class, param);
-        System.out.println("test");
+        ResponseEntity<Country[]> response = restTemplate.exchange(url, HttpMethod.GET, request, Country[].class);
 
         return response.getBody();
-    }
-
-    public String getForecastImageUrl(String city, String country) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("x-rapidapi-forward-key", "b61a5a7435msh866977d946919afp1c7620jsn64158120fd4f");
-        headers.add("X-RapidAPI-Key", "b61a5a7435msh866977d946919afp1c7620jsn64158120fd4f");
-        headers.add("X-RapidAPI-Host", "weather-embed.p.rapidapi.com");
-
-        WeatherLocation weatherLocation = new WeatherLocation(city, country);
-
-        HttpEntity<WeatherLocation> request = new HttpEntity<>(weatherLocation, headers);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity("https://weather-embed.p.rapidapi.com/forecast/create", request, Map.class);
-
-        return (String) response.getBody().get("url");
     }
 }
